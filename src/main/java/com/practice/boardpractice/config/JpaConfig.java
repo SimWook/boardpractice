2b39d3e931445396f9de0d7ce8ed9e3f802cf15b
シム ウク <astralkaiser89@gmail.com>
2024-05-10 10:13:33 +0900

    part1 commit

---
+++ src/main/java/com/practice/boardpractice/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.practice.boardpractice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("uno"); // TODO: spring security認証機能をつけるときに、修正しよう
+    }
+}
