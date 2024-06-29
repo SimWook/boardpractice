69fb073186e1a604dfa821ca6583db8660085f04
シム ウク <astralkaiser89@gmail.com>
2024-06-29 23:33:27 +0900

    #5 thymeleaf view controller作成

---
+++ src/main/java/com/practice/boardpractice/controller/ArticleController.java
@@ -0,0 +1,15 @@
+package com.practice.boardpractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * /articles
+ * /articles/{articles-id}
+ * /articles/search
+ * /articles/search-hashtag
+ */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
---
+++ src/test/java/com/practice/boardpractice/controller/ArticleControllerTest.java
@@ -0,0 +1,52 @@
+package com.practice.boardpractice.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View Controller: Article")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+    private final MockMvc mockMvc;
+
+    public ArticleControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @DisplayName("[view][GET] 掲示板リストページ - 正常パターン")
+    @Test
+    public void get_article_list_success() throws Exception {
+        mockMvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("article"));
+    }
+
+    @DisplayName("[view][GET] 掲示板1件取得ページ - 正常パターン")
+    @Test
+    public void get_article_one_success() throws Exception {
+        mockMvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("article"));
+    }
+
+    @DisplayName("[view][GET] 掲示板検索ページ - 正常パターン")
+    @Test
+    public void get_article_list_search() throws Exception {
+        mockMvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("[view][GET] 掲示板ハッシュタグ検索ページ - 正常パターン")
+    @Test
+    public void get_article_list_hashtag() throws Exception {
+        mockMvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+}
