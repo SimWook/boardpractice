1f3683936fa353148730550ee6211ea71e275913
シム ウク <astralkaiser89@gmail.com>
2024-06-29 23:46:45 +0900

    #5 テストコード非活性化

--- src/test/java/com/practice/boardpractice/controller/ArticleControllerTest.java
+++ src/test/java/com/practice/boardpractice/controller/ArticleControllerTest.java
@@ -1,5 +1,6 @@
 package com.practice.boardpractice.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,6 +10,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Disabled("作成中")
 @DisplayName("View Controller: Article")
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
@@ -23,6 +25,7 @@ class ArticleControllerTest {
     public void get_article_list_success() throws Exception {
         mockMvc.perform(get("/articles"))
                 .andExpect(status().isOk())
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("article"));
     }
 
@@ -31,7 +34,9 @@ class ArticleControllerTest {
     public void get_article_one_success() throws Exception {
         mockMvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(model().attributeExists("article"));
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
     }
 
     @DisplayName("[view][GET] 掲示板検索ページ - 正常パターン")
@@ -39,7 +44,8 @@ class ArticleControllerTest {
     public void get_article_list_search() throws Exception {
         mockMvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles/search"));
     }
 
     @DisplayName("[view][GET] 掲示板ハッシュタグ検索ページ - 正常パターン")
@@ -47,6 +53,7 @@ class ArticleControllerTest {
     public void get_article_list_hashtag() throws Exception {
         mockMvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
